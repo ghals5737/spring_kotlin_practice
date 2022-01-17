15608fe3a57444232b5d18554acac4ddd38f22eb
ghals <ghals5737@naver.com>
2022-01-17 23:32:01 +0900

    지역 검색 이미지 검색 serviceTest 추가

---
+++ mvc/src/test/kotlin/com/example/mvc/service/WishServiceTest.kt
@@ -0,0 +1,18 @@
+package com.example.mvc.service
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WishServiceTest {
+
+    @Autowired
+    lateinit var wishService: WishService
+
+    @Test
+    fun WishServiceTest(){
+
+    }
+
+}
